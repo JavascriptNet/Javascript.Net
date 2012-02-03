7ef942c606d9b88b0d54ea5ae51d86d697dae507
SND\oliverbock_cp <SND\oliverbock_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2012-02-03 00:41:20 +0000

    Imported from Noesis.Javascript-v0.4-Source.zip
    git-svn-id: https://javascriptdotnet.svn.codeplex.com/svn@73921 9b283d60-5439-405e-af05-b73fd8c4d996

---
+++ release_zip/0.4/Source/Noesis.Javascript/JavascriptException.cpp
@@ -0,0 +1,109 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// File: JavascriptException.cpp
+// 
+// Copyright 2010 Noesis Innovation Inc. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "JavascriptException.h"
+
+#include "JavascriptInterop.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using namespace v8;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace Noesis { namespace Javascript {
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+JavascriptException::JavascriptException(TryCatch& iTryCatch): System::Exception(GetExceptionMessage(iTryCatch), GetSystemException(iTryCatch))
+{
+	v8::Local<v8::Message> message = iTryCatch.Message();
+	if (!message.IsEmpty())
+	{
+		mSource = gcnew System::String((wchar_t*) *String::Value(message->GetScriptResourceName()));
+		mLine = message->GetLineNumber();
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+System::String^
+JavascriptException::Source::get()
+{
+	return mSource;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int
+JavascriptException::Line::get()
+{
+	return mLine;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+System::String^
+JavascriptException::GetExceptionMessage(TryCatch& iTryCatch)
+{
+	System::String^ location;
+
+	v8::Local<v8::Message> message = iTryCatch.Message();
+	if (!message.IsEmpty())
+		location = gcnew System::String((wchar_t*) *String::Value(message->GetScriptResourceName())) + ", line " + message->GetLineNumber();
+	else
+		location = gcnew System::String("Unknown location");
+	
+	System::Exception^ exception = GetSystemException(iTryCatch);
+	if (exception != nullptr)
+		return gcnew System::String("Exception in managed code invocation (" + location + ").");
+	else
+		return gcnew System::String((wchar_t*) *String::Value(iTryCatch.Exception())) + "(" + location + ")";
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+System::Exception^
+JavascriptException::GetSystemException(TryCatch& iTryCatch)
+{
+	v8::Local<v8::Value> v8exception = iTryCatch.Exception();
+
+	if (JavascriptInterop::IsSystemObject(v8exception))
+	{
+		System::Object^ object = JavascriptInterop::UnwrapObject(v8exception);
+		return dynamic_cast<System::Exception^>(object);
+	}
+
+	return nullptr;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+} } // namespace Noesis::Javascript
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ release_zip/0.4/Tests/Noesis.Javascript.Tests/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using System.Collections;
+
+using Noesis.Javascript;
+
+namespace Noesis.Javascript.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Regression Tests
+            JavascriptTest javascriptTest = new JavascriptTest();
+            javascriptTest.RunRegressionTests();
+        }
+    }
+}
