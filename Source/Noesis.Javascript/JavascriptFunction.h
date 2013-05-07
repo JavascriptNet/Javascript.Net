bdf6cd62e29c6ae718bb9588c1176ae2b71583ce
Tom Rathbone <tom.rathbone@gmail.com>
2013-05-07 16:50:35 +0100

    Implement handling of JavascriptFunction

---
+++ Source/Noesis.Javascript/JavascriptFunction.h
@@ -0,0 +1,45 @@
+#pragma once
+
+//////////////////////////////////////////////////////////////////////////
+
+#include <v8.h>
+
+#include "JavascriptContext.h"
+
+using namespace v8;
+
+//////////////////////////////////////////////////////////////////////////
+
+namespace Noesis { namespace Javascript {
+
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+// JavascriptFunction
+//
+// Wraps around JS function object and allow calling it in later time
+//////////////////////////////////////////////////////////////////////////
+public ref class JavascriptFunction
+{
+public:
+	JavascriptFunction(v8::Handle<v8::Object> iFunction, JavascriptContext^ context);
+	~JavascriptFunction();
+	!JavascriptFunction();
+
+	System::Object^ Call(... cli::array<System::Object^>^ args);
+
+	static bool operator== (JavascriptFunction^ func1, JavascriptFunction^ func2);
+	bool Equals(JavascriptFunction^ other);
+	
+	virtual bool Equals(Object^ other) override;
+
+private:
+	v8::Persistent<v8::Function>* mFuncHandle;
+	JavascriptContext^ mContext;	
+};
+
+//////////////////////////////////////////////////////////////////////////
+
+} } // namespace Noesis::Javascript
+
+//////////////////////////////////////////////////////////////////////////
