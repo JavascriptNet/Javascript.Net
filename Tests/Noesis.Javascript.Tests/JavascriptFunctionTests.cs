2890fe3fef56cd4345a2458473992b6b9f4e5ced
Tom Rathbone <tom.rathbone@gmail.com>
2013-05-13 16:00:36 +0100

    rebase and fix up

---
+++ Tests/Noesis.Javascript.Tests/JavascriptFunctionTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Noesis.Javascript.Tests
+{
+    [TestFixture]
+    public class JavascriptFunctionTests
+    {
+        [Test]
+        public void GetFunctionFromJsContext()
+        {            
+            JavascriptContext context = new JavascriptContext();
+            context.Run("a = function(a,b) {return a+b;}");
+            
+            JavascriptFunction funcObj = context.GetParameter("a") as JavascriptFunction;
+
+            Assert.That(funcObj, Is.Not.Null);
+
+            object result = funcObj.Call(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));            
+        }
+    }
+}
