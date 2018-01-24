51d8330332d3fe844bc82e1987dd5da6c0d46080
Oliver Bock <oliver@g7.org>
2018-01-24 11:36:11 +1100

    Fixed full internationalization support by distributing icudtl.dat too.

---
+++ Tests/Noesis.Javascript.Tests/InternationalizationTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Noesis.Javascript.Tests
+{
+    [TestClass]
+    public class InternationalizationTests
+    {
+        [TestMethod]
+        public void BasicButNotFullIcuAvailable()
+        {
+            using (var context = new JavascriptContext()) {
+                // Confirm that internationalization support works.
+                // (I can find no way to disable this, so you must distribute icudtl.dat.)
+                context.Run(@"
+const january = new Date(9e8);
+const spanish = new Intl.DateTimeFormat('es', { month: 'long' });
+spanish.format(january) === 'enero';").Should().BeOfType<bool>().Which.Should().BeTrue("it should be able to convert months into foreign languages");
+            }
+        }
+    }
+}
