e2933d99c5b14dc38fa6a16d2a4ab97178056711
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2018-06-13 08:30:17 +0200

    Update V8 to version 6.8.275.2

--- Source/Noesis.Javascript/JavascriptException.cpp
+++ Source/Noesis.Javascript/JavascriptException.cpp
@@ -150,7 +150,7 @@ JavascriptException::GetSystemException(TryCatch& iTryCatch)
 	if (v8exception->IsObject()) {
 		v8::Handle<v8::Object> exception_o = v8::Handle<v8::Object>::Cast(v8exception);
 		v8::Handle<v8::String> inner_exception_str = v8::String::NewFromUtf8(JavascriptContext::GetCurrentIsolate(), "InnerException", v8::NewStringType::kNormal).ToLocalChecked();
-		if (exception_o->HasOwnProperty(inner_exception_str)) {
+		if (exception_o->HasOwnProperty(JavascriptContext::GetCurrentIsolate()->GetCurrentContext(), inner_exception_str).FromMaybe(false)) {
 			v8::Handle<v8::Value> inner = exception_o->Get(inner_exception_str);
 			System::Object^ object = JavascriptInterop::UnwrapObject(inner);
 			return dynamic_cast<System::Exception^>(object);
