80cd319f146e791fd4c6c57f640b9cab058c342a
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2018-09-04 13:50:57 +0200

    Provide static method to set engine flags

---
+++ Tests/Noesis.Javascript.Tests/FlagsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Noesis.Javascript.Tests
+{
+    [TestClass]
+    public class FlagsTest
+    {
+        [TestMethod]
+        public void CanUseEngineFlagsToSpecifyStrictMode()
+        {
+            JavascriptContext.SetFlags("--use_strict");
+            Action action = () =>
+            {
+                using (var context = new JavascriptContext())
+                    context.Run("globalVariable = 1;");
+            };
+            action.ShouldThrowExactly<JavascriptException>().WithMessage("ReferenceError: globalVariable is not defined");
+            JavascriptContext.SetFlags("--nouse_strict");
+        }
+    }
+}
